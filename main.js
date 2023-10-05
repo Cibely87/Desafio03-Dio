c60ce4e8e7d16c41833d2fa56ace8057a862a15f
Cibely87 <cibely.port@gmail.com>
2023-10-05 09:05:19 -0300

    perf/finalizando o desafio

--- main.js
+++ main.js
@@ -1,29 +1,37 @@
-class hero {
+class heroi {
 
-    constructor(name, age, type) {
-        name = this.name;
-        age = this.age;
-        type = this.type;
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+        let ataque;
+        switch (this.tipo) {
+            case "Mago": ataque = "Magia";
+                break;
+            case "Guerreiro": ataque = "Espada";
+                break;
+            case "Monge": ataque = "Artes Marciais";
+                break;
+            case "Ninja": ataque = "Shuriken";
+                break;
+            default:
+                ataque = "Realizou um ataque"
+        }
+        console.log(`O ${this.tipo} atacou usando ${ataque}`)
     }
 }
 
-attack() {
+const heroiPotiguar = new heroi("Joao", 26, "Guerreiro")
+const heroiBaiano = new heroi("Severino", 55, "Ninja")
+const heroiSergipano = new heroi("José", 39, "Monge")
+const heroiPernambucano = new heroi("Francisco", 21, "Mago")
 
-    let goAttack;
 
-    switch (this.type) {
-        case 'Mage':
-            goAttack = 'Magic';
-        case 'warrior':
-            goAttack = 'sword';
-        case 'monk':
-            goAttack = 'martial arts';
-        case 'ninja':
-            goAttack = 'shuriken';
-        default:
-            goAttack = 'made an attack';
-    }
-
-    console.log(`The ${this.type}attacked using ${attack}`);
-}
+heroiPotiguar.atacar();
+heroiBaiano.atacar();
+heroiSergipano.atacar();
+heroiPernambucano.atacar();
 
